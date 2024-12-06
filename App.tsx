275f77d159a4df459ef13ba72b4787f91052cfc4
Renan Mello <renanbmello@gmail.com>
2024-12-06 12:38:59 -0300

    fix: home screen navigation

--- App.tsx
+++ App.tsx
@@ -3,13 +3,14 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { ActivityList } from './src/components/ActivityList';
 import ActivityDetail from './src/components/ActivityDetail';
 import { RootStackParamList } from './src/types/navigation';
-
+import { HomeScreen } from './src/components/HomeScreen';
 const Stack = createStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="ActivityList">
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="ActivityList" component={ActivityList} />
         <Stack.Screen name="ActivityDetail" component={ActivityDetail} />
       </Stack.Navigator>
--- src/types/navigation.ts
+++ src/types/navigation.ts
@@ -1,4 +1,5 @@
 export type RootStackParamList = {
+  Home: undefined;
   ActivityList: undefined;
   ActivityDetail: { id: number };
 };
