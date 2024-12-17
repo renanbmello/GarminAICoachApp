987ecad0620f3fe06ed03c4bb3148cb9a0b43c51
Renan Mello <renanbmello@gmail.com>
2024-12-17 12:02:36 -0300

    fix: list styles

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,39 +1,41 @@
 import React from 'react';
-import { FlatList, StyleSheet } from 'react-native';
+import { FlatList, StyleSheet, View, SafeAreaView } from 'react-native';
 import { Activity } from '../types/activity';
 import { ActivityCard } from './ActivityCard';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../types/navigation';
 import { useActivities } from '../hooks/useActivities';
+import { useTheme } from '../context/ThemeContext';
+import { colors } from '../theme/colors';
+import { activityListStyles as styles } from '../styles/components/activityList.styles';
 
 type ActivityListNavigationProp = NativeStackNavigationProp<RootStackParamList, 'ActivityList'>;
 
 export const ActivityList: React.FC = () => {
   const navigation = useNavigation<ActivityListNavigationProp>();
   const { activities } = useActivities();
+  const { theme } = useTheme();
 
   const handleActivityPress = (activity: Activity) => {
     navigation.navigate('ActivityDetail', { id: activity.id });
   };
 
   return (
-    <FlatList
-      data={activities}
-      keyExtractor={(item) => item.id.toString()}
-      renderItem={({ item }) => (
-        <ActivityCard 
-          activity={item} 
-          onPress={() => handleActivityPress(item)} 
-        />
-      )}
-      contentContainerStyle={styles.listContent}
-    />
+    <SafeAreaView style={styles(theme).container}>
+      <FlatList
+        data={activities}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={styles(theme).cardWrapper}>
+            <ActivityCard 
+              activity={item} 
+              onPress={() => handleActivityPress(item)} 
+            />
+          </View>
+        )}
+        contentContainerStyle={styles(theme).listContent}
+      />
+    </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  listContent: {
-    paddingBottom: 20,
-  },
-}); 
